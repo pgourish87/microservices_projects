df9d0f60888ac07c82126260541999675b2d1500
pgourish87 <pgourish012@gmail.com>
2024-02-13 19:21:45 +0530

    initial project setup done for Microservice Using Spring Boot & Spring Cloud

---
+++ order-service/src/main/java/com/gbp/orderservice/api/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.gbp.orderservice.api.controller;
+
+import com.gbp.orderservice.api.entity.Order;
+import com.gbp.orderservice.api.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @PostMapping("/bookOrder")
+    public Order bookOrder(@RequestBody Order order) {
+        return service.saveOrder(order);
+    }
+
+}
